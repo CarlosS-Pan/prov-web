fc0f14450f6ff0d2a17283584f27e3ed967bcc51
Carlos Sanabria <carlossanabria636@gmail.com>
2023-09-08 13:24:26 -0400

    Linking front and back plus a little test

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,12 +1,26 @@
+# Import flask and datetime module for showing date and time
 from flask import Flask
+import datetime
 
+date = datetime.datetime.now()
+
+# Initializing flask app
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello from Flask!'
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+
+    # Returning an api for showing in reactjs
+    return {
+        'Name': "Carlos",
+        "Age": "23",
+        "Date": date,
+        "Language": "python"
+    }
 
 
+# Running app
 if __name__ == '__main__':
     app.run(debug=True)

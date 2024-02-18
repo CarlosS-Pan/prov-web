7e450386848c0f7a3c76cc843dd1e64c906c6416
Carlos Sanabria <carlossanabria636@gmail.com>
2024-02-18 18:08:35 -0500

    moving small changes to new computer

--- client/src/components/about-us/AboutUs.jsx
+++ client/src/components/about-us/AboutUs.jsx
@@ -4,9 +4,20 @@ const AboutUs = () => {
     return ( 
         <>
             <div className='content-container'>
-                <h1>
-                    about us si
+                <h1 className='content-title'>
+                    Who we are
                 </h1>
+                <div className="description-container">
+                    About us summary message. 
+                    The incorporation of different 
+                    illustrations that portray the inherent value
+                     of their business name is very remarkable. 
+                     Try to Design your About us page template in a way that its 
+                     magic of creativity can make your visitor clumped to it. 
+                </div>
+            </div>
+            <div className="photos">
+                ola amigos de yt
             </div>
         </>
     );

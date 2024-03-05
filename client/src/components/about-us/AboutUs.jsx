f1989f3ead93a36424ab8580ed687e395dd2c07a
Carlos Sanabria <carlossanabria636@gmail.com>
2024-03-04 23:50:49 -0500

    positioning the divs in their potential final layout; font size adjustment and possibly typeface change pending

--- client/src/components/about-us/AboutUs.jsx
+++ client/src/components/about-us/AboutUs.jsx
@@ -1,23 +1,47 @@
 import './about-us.css'
+import { p1 } from '../../assets/index'
 
 const AboutUs = () => {
     return ( 
         <>
             <div className='content-container'>
                 <h1 className='content-title'>
-                    Who we are
+                    Who we are ?
                 </h1>
-                <div className="description-container">
-                    About us summary message. 
-                    The incorporation of different 
-                    illustrations that portray the inherent value
-                     of their business name is very remarkable. 
-                     Try to Design your About us page template in a way that its 
-                     magic of creativity can make your visitor clumped to it. 
+                <div className="summary-container">
+                    <p>
+                    Welcome to SmartReefer, your trusted partner 
+                    in refrigerated unit repairs and premier supplier 
+                    of spare parts for trailers and reefers.
+                    </p>
                 </div>
             </div>
-            <div className="photos">
-                ola amigos de yt
+            <div className="extended-container">
+                <div className="extended-content">
+                    {/* <h2 className="extended-title"> */}
+                    <h2>
+                    <span className="esteinline"> A Decade of Excellence </span>
+                    in Refrigerated Unit Repairs and Parts Distribution.
+                    </h2>
+                    {/* <p className="extended-description"> */}
+                    <p>
+                        For a decade, SmartReefer has led Florida's 
+                        refrigerated transportation. 
+                        Specializing in trailer refrigerated unit repairs, 
+                        our excellence extends to seamless supply chains 
+                        through key partnerships.
+                    </p>
+                    <p>
+                        Fluent in English and Spanish, our team drives 
+                        reliability, offering solutions beyond repairs. 
+                        At SmartReefer, we shape the future of refrigerated 
+                        transportation with experience and dedication to keep your 
+                        business moving forward
+                    </p>
+                </div>        
+                <div className="extended-photo">
+                <img src={ p1 } alt="choppa" />
+                </div>
             </div>
         </>
     );
